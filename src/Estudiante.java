093ba1d0294e0899f35515560279dcf0fb6f911b
dylan <dylnenatoxd@gmail.com>
2024-05-07 07:53:37 -0500

    Herencia -Implementacion de propiedad adicional para el grado por Dylan

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,5 +1,7 @@
 public class Estudiante extends Persona{
 
+    private  String Grado;
+
     public Estudiante(String Nombre, Integer Edad, String FechaDeNacimiento, String Grado){
         super (Nombre, Edad, FechaDeNacimiento);
         this.Grado = Grado;
