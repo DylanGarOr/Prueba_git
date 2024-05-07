1099bc1b8b7adcf8da86ff9203e05fceacf84fa6
dylan <dylnenatoxd@gmail.com>
2024-05-07 07:22:38 -0500

    Encapsulamiento -Implementacion de Metodos de Propiedades por Dylan

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,27 @@
 public class Persona {
     private String Nombre;
-    private String FechaDeNacimiento;
     private Integer Edad;
+    private String FechaDeNacimiento;
+
+    public Persona(String Nombre, Integer Edad, String FechaDeNacimiento){
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.FechaDeNacimiento = FechaDeNacimiento;
+    }
+
+    public String obtenerNombre(){
+        return Nombre;
+    }
+    public void establecerNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+    public Integer obtenerEdad(){
+        return Edad;
+    }
+    public String obtenerFechaDeNacimiento(){
+        return FechaDeNacimiento;
+    }
+    public  void establecerFechaDeNacimiento(String FechaDeNacimiento){
+        this.FechaDeNacimiento = FechaDeNacimiento
+    }
 }
