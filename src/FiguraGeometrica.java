caeb6bc43d9b00ca7ba558fe7a39cc3875eacd7c
dylan <dylnenatoxd@gmail.com>
2024-05-08 20:24:23 -0500

    Encapsulamiento y Herencia implementacion de clase FiguraGeometrica por Dylan

---
+++ src/FiguraGeometrica.java
@@ -0,0 +1,9 @@
+public class FiguraGeometrica {
+    private String Color;
+    private String Nombre;
+
+    public FiguraGeometrica(String Color, String Nombre){
+        this.Color = Color;
+        this.Nombre = Nombre;
+    }
+}
