c4362ff0cecc1c8c7046689a09f39314ccdbc2e8
dylan <dylnenatoxd@gmail.com>
2024-05-08 20:31:26 -0500

    Encapsulamiento y Herencia implementacion de metodos para manipulacion de propiedades por Dylan

--- src/FiguraGeometrica.java
+++ src/FiguraGeometrica.java
@@ -6,4 +6,18 @@ public class FiguraGeometrica {
         this.Color = Color;
         this.Nombre = Nombre;
     }
+    public String obtenerColor(){
+        return Color;
+    }
+
+    public void establecerColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String obtenerNombre(){
+        return Nombre;
+    }
+    public void establecerNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
 }
