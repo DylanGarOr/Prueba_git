11c41889b568d882cff911c3f20f6b1fa4315a16
dylan <dylnenatoxd@gmail.com>
2024-05-08 20:43:52 -0500

    Encapsulamiento y Herencia Implementacion de Subclases por Dylan

---
+++ src/Circulo.java
@@ -0,0 +1,9 @@
+public class Circulo extends FiguraGeometrica{
+
+    private double Radio;
+
+    public Circulo(String Nombre, String Color, double Radio) {
+        super(Nombre, Color);
+        this.Radio = Radio;
+    }
+}
---
+++ src/Rectangulo.java
@@ -0,0 +1,12 @@
+public class Rectangulo extends FiguraGeometrica {
+
+    private double Ancho;
+    private double Longitud;
+
+    public Rectangulo(String Nombre, String Color, double Ancho, double Longitud) {
+        super(Nombre, Color);
+        this.Ancho = Ancho;
+        this.Longitud = Longitud;
+    }
+}
+
